7aef7fb833210536b9ba6efea915c066f915b988
Razi Ahmad <raziahmad854301@gmail.com>
2025-08-11 07:54:34 +0530

    enhance after 19 june

--- j.js
+++
@@ -1,6 +0,0 @@
-const form = document.getElementById("gym-membership-form");
-const button = document.getElementById("open-form-button");
-
-button.addEventListener("click", function() {
-  form.style.display === "block" ? form.style.display = "none" : form.style.display = "block";
-});
